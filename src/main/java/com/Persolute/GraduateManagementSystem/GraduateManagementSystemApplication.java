a2938e7a057b0cd9752e0438460515a477860adb
Persolute <1538520381@qq.com>
2025-01-15 12:19:06 +0800

    基本配置

---
+++ src/main/java/com/Persolute/GraduateManagementSystem/GraduateManagementSystemApplication.java
@@ -0,0 +1,18 @@
+package com.Persolute.GraduateManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Persolute
+ * @version 1.0
+ * @description 启动类
+ * @email 1538520381@qq.com
+ * @date 2025/01/15 12:02
+ */
+@SpringBootApplication
+public class GraduateManagementSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GraduateManagementSystemApplication.class, args);
+    }
+}
